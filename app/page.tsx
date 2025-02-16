6431b7d0cb15793e8aa7b8eb3ea9dc614f531460
Andrew Vitrenko <avitrenko2004@gmail.com>
2025-02-16 19:10:12 +0100

    feat: improved code style

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
+import { FC, memo } from 'react';
 
-export default function Home() {
+const Home: FC = memo(() => {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
@@ -98,4 +99,8 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+});
+
+Home.displayName = 'Home';
+
+export default Home;
